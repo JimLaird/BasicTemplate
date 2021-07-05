7292ccf8165fdaa064c393183e4e312880bfbfc4
JimLaird <jimlaird01@gmail.com>
2021-07-05 13:57:04 +0100

    Add project files.

---
+++ BasicTemplate/BasicTemplate/ViewModels/SettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicTemplate.ViewModels
+{
+    public class SettingsModel : ViewModelBase
+    {
+        public SettingsModel()
+        {
+            Title = "Settings";
+
+        }
+    }
+}
